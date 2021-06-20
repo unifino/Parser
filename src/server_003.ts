c536f60bd94ddc8dc673dd23333ce80b17703472
unifino <unifino@protonmail.com>
2021-06-20 07:20:11 +0430

    نهج‌الفصاحة

---
+++ src/server_003.ts
@@ -0,0 +1,11 @@
+import * as fs                          from "fs";
+import * as storage                     from "./storage";
+import * as TS                          from "./types";
+
+let name = "نهج‌الفصاحة";
+export function ignite () {
+
+    console.log(name);
+    
+
+}

16b0af894a2dda016876094ef973ac98a4aad5d5
unifino <unifino@protonmail.com>
2021-07-19 21:21:34 +0430

    ...

--- server/مستدرك‌الوسائل.ts
+++ server/مستدرك‌الوسائل.ts
@@ -68,10 +68,11 @@ function load_db_v0 ( mode: "Scratch"|"Cached" ) {
         fs.rmSync( db_v0_Path, { force: true } );
 
         let textBook: string = "";
-        let tmp_db: TS.db = [];
 
         // .. merge Books as Source
-        textBook += " " + readSrcBook( "source/مستدرك‌الوسائل/base.html" );
+        textBook += " " + readSrcBook( "source/مستدرك‌الوسائل/base-00.html" );
+        // .. some internal edits
+        textBook = my_some_edits( textBook );
         // ..  do some edits
         textBook = __.some_edits( textBook );
         // .. remove unwanted parted => neat
@@ -79,24 +80,17 @@ function load_db_v0 ( mode: "Scratch"|"Cached" ) {
         // .. build first db => rawDB
         let raw_db = getRawDB( textBook );
         // // .. provide a preview for checking
-        // preview_1( raw_db.join("</p>") );
+        // preview_1( raw_db.join( "</p>" ) );
         // .. build main db
-        tmp_db = hadith_db_generator( raw_db );
+        db_v0 = hadith_db_generator( raw_db );
         // .. scatter 0-a-9
-        tmp_db = a_0_9( tmp_db );
-        storage.saveData( tmp_db, "tmp", "cdn" );
+        db_v0 = a_0_9( db_v0);
+            console.log(db_v0.length);
+            storage.saveData( db_v0, "tmp", "ctl" )
         // .. provide a preview for checking
-        let tmp = "";
-        for ( let p of tmp_db ) tmp += "<p>" + p.d + p.tmp.w.join( " " ) + "</p>";
-
-        preview_1( tmp );
+        preview_1( tmpProvider_2( db_v0 ) );
         // // .. assign c code
-        // tmp_db = c_allocator( tmp_db );
-        // // .. sum up Books
-        // db_v0 = [ ...db_v0, ...tmp_db ];
-
-        // // .. provide a preview for checking
-        // preview_2( db_v0 );
+        // db_v0 = c_allocator( db_v0 );
 
     }
 
@@ -119,6 +113,27 @@ function readSrcBook ( link: string ): string {
 
     if ( a>0 && b>0 ) return txt.slice( a+6, b );
     else console.log( "err-01",a , b )
+
+}
+
+// .. ====================================================================
+
+function my_some_edits ( textBook: string ) {
+
+    let q = textBook.match( /<span class="fm">(.*?)<\/span>/g ) || [];
+    console.log(q.length);
+    let i  = 0;
+    let time= new Date().getTime();
+    for ( let c of q ) {
+        if ( !(i%100)) report.timer(i,q.length,time,5); i++;
+        let r = c;
+        r = r.replace( "<span class=\"fm\">", " " );
+        r = r.replace( "<\/span>", " " );
+        textBook = textBook.replace( c, r );
+    }
+    console.log("done");
+
+    return textBook;
 
 }
 
@@ -174,8 +189,8 @@ function removeAlaemTags ( text: string ) {
     let q = ( text.match( /<span class="quran">(.*?)<\/span>/g ) || [] );
     for ( let c of q ) {
         let r = c;
-        r = r.replace( "<span class=\"quran\">", " <Q> " );
-        r = r.replace( "<\/span>", " </Q> " );
+        r = r.replace( "<span class=\"quran\">", " |Q| " );
+        r = r.replace( "<\/span>", " |/Q| " );
         text = text.replace( c, r );
     }
 
@@ -266,19 +281,46 @@ function a_0_9 ( db: TS.db ) {
 
     // ! الْخَبَرَ
     // ! إلخ
-    // ! م
-    // ! 12273 check if ajmaein?
+    // ! مَا يَقْرُبُ مِنْهُ
+    // ! ]
+    // ! [
+    // ! |Q| & in 0
+    // ! check: 173 610...q | 1040 ...q
+    // ! check again : 1877
+    // ! class="sher": 7563,9199,9208,13091,14673,14796,18435,20368,
 
     let container: { [key: string]: TS.db_item[] } = {};
+    let cdnBox_pre: number[];
 
     for( let p of db ) {
 
-        // .. module A
-        if ( p.tmp.w.length === 1 )
-            if ( p.tmp.w[0].includes( "مِثْل" ) || p.tmp.w[0].includes( "أَيْضاً" ) ) {
+        // .. module A : merge
+        cdnBox_pre = [42];
+        if ( cdnBox_pre.includes( Number(p.d) ) ) {
+            p.a = p.tmp.w.join( " " );
+        }
+
+        // .. module A : merge
+        cdnBox_pre = [7049,7054,12500,12502,14066,2692,2161];
+        if ( cdnBox_pre.includes( Number(p.d) ) ) {
+            p.a = p.tmp.w[0];
+        }
+        else if ( p.tmp.w.length === 1 ) {
+            cdnBox_pre = [
+                18325,18550,23041,18571,18777,18876,18939,8818,10042,
+                16539,1219
+            ];
+            if
+            (
+                p.tmp.w[0].includes( "مِثْل" ) || 
+                p.tmp.w[0].includes( "أَيْضاً" ) ||
+                cdnBox_pre.includes( Number(p.d) )
+            )
+            {
                 p[0] = p.tmp.w[0];
-                p.a = "remove it";
+                p.a = "";
             }
+        }
 
         // .. module S - H
         if ( p.tmp.w.length === 2 )
@@ -313,7 +355,6 @@ function a_0_9 ( db: TS.db ) {
                     p[9] = p.tmp.w[2];
                 }
 
-        // ! check again : 1877
         // .. module S - H - Q - H
         if ( p.tmp.w.length === 4 ) {
             if ( !p.tmp.w[0].includes( "class" ) )
@@ -324,107 +365,175 @@ function a_0_9 ( db: TS.db ) {
                     }
         }
         // .. module S - H - S - A
-        if ( p.tmp.w.length === 4 ) {
-            if ( !p.tmp.w[0].includes( "class" ) && !p.tmp.w[2].includes( "class" ) )
-                if ( p.tmp.w[1].includes( "hadith" ) && p.tmp.w[3].includes( "hadith" ) ) 
-                    if ( !p.tmp.w[2].includes("Q") ) {
-                        p[0] = p.tmp.w[0];
-                        p.a = p.tmp.w[1];
-                        p[9] = p.tmp.w[2] + " " + p.tmp.w[3];
-                    }
-        }
+        // ! join: 443,465,466,927,1679,2173,2929,2973,3654,4177,4208,4298,
+        // ! 4353,4358,4479,4670,4713,4793,4806,4855,4957,4978,5240,5279,
+        // ! 5299,5301,5376,5571,6284,6519,6604,6674,6718,6741,6818,6860,
+        // ! 6868,6962,7005,7019,7068,7139,7177,7402,7465,7645,7933,8025,
+        // ! 8188,8450,8503,8682,8880,8991,9024,9048,9264,مَا يَقْرَبُ مِنْهُ
+        // ! 9314,9359,9391,9698,الْحَدِيثَ
+        // ! 10147,10234,10387,10682,10881,10886,10942,11370,11915,12002,
+        // ! 12073,12102,12134,12199,12227,12257,12467,12672,12928,13164,
+        // ! 13210,13221,13249,13495,13521,13611,13622,13832,13925,14322,
+        // ! 15192,15440,16117,16169,16232,16298,16351,16420,16550,16650,
+        // ! 16661,16686,16704,17027,17420,17508,17537,17541,17907,18048,
+        // ! 18113,18215,19123,19398,19502,19693,19777,19850,20181,20271,
+        // ! 20561,20594,20875,20901,20925,20954,21281,21365,21367,21387,
+        // ! 21669,21752,21822,22048,22402,22813,22815,22825,23109
 
-        // ! .. control by hand: 7851 7859
-        let cdnBox_1 = [ 
-            473,574,681,688,1143,1298,1791,3175,3186,3236,3558,3598,3831,
-            4177,4222,4790,4800,4977,5387,5390,6129,6287,6750,6822,7106,
-            7310,7421,7851,8286,8292,8987,8997,9168,9171,9757,9772,9775,
-            9876,9980,10155,12263,16358,18100,7117,16548,16846,18608,
-            22235
-        ];
 
-        if ( cdnBox_1.includes( Number(p.d) ) ) {
-            p[0] = p.tmp.w.splice(0,1).join( " " );
-            p.a = p.tmp.w.splice(0,1).join( " " );
-            p[9] = p.tmp.w.join( " " );
-        };
+        // // ! check 602,1270,7535,8808,9556,12352,13404,13413,13443,14372,
+        // // ! 14670,14904,18046 HinS,20541,22238,
+        // if ( p.tmp.w.length === 4 )
+        //     if ( !p.tmp.w[0].includes( "class" ) && !p.tmp.w[2].includes( "class" ) )
+        //         if ( p.tmp.w[1].includes( "hadith" ) && p.tmp.w[3].includes( "hadith" ) ) 
+        //             if ( !p.tmp.w[2].includes("Q") ) {
+        //                 p[0] = p.tmp.w[0];
+        //                 p.a = p.tmp.w[1];
+        //                 p[9] = p.tmp.w[2] + " " + p.tmp.w[3];
+        //             }
 
-        let cdnBox_2 = [
-            91,108,214,325,343,730,845,957,1096,1106,1111,1125,1137,1194,
-            1264,1301,1355,1359,1363,1425,1435,1444,1563,1669,1686,1692,
-            1707,1777,1784,1876,1877,1987,2098,2177,2188,2328,2348,2360,
-            2374,2405,2467,2514,2516,2529,2559,2560,2568,2653,2671,2911,
-            2978,3003,3074,3093,3098,3122,3125,3132,3148,3222,3252,3400,
-            3428,3449,3455,3509,3536,3559,3579,3627,3669,3704,3951,4202,
-            4360,4746,5016,5130,5337,5576,5870,6098,6200,6260,6569,7082,
-            7084,7201,7264,7372,7521,7554,7560,7690,7918,8116,8174,8308,
-            8432,8497,8524,8669,8744,8815,8842,8888,10027,10073,10121,
-            11152,11160,11519,11674,11741,11804,11876,11914,11930,12182,
-            12193,12232,12293,12295,12313,12738,12751,12752,12769,12778,
-            12830,12846,12911,12987,13015,13069,13138,13159,13208,13238,
-            13217,13146,13125,13115,13376,13388,13401,13422,13463,13481,
-            13512,13527,13528,13541,13544,13558,13567,13578,13588,13592,
-            13623,13627,13629,13633,13639,14328,14339,14437,14516,14529,
-            14563,14662,14695,14703,14724,14908,15119,15198,15342,15974,
-            16350,16384,16416,16621,16658,16687,16856,17362,17908,17919,
-            17924,17933,17955,18039,18080,18083,18092,18095,18127,18128,
-            18143,18183,18203,18207,18217,19134,19306,19553,19593,19627,
-            19634,19651,19652,19695,19733,19947,19986,20052,20199,20213,
-            20260,20266,20303,20383,20385,20491,20515,20518,20776,20796,
-            20815,21100,21482,21601,22005,22233,22439,22607,22728,
-            13283,7093,7095,7442,7859,12649,12931,12954,12963,12965,
-            13079,13224,13283,13342,13351,13366,13488,13668,13671,13674,
-            13685,13686,13689,13699,13707,13731,13753,13761,13762,13833,
-            13837,13895,13945,13947,13954,14043,14202,14244,14252,14263,
-            14269,14277,14281,14285,14303,15587,19355,19940,
-        ];
+    //     // ! .. control by hand: 7851 7859
+    //     let cdnBox_1 = [ 
+    //         473,574,681,688,1143,1298,1791,3175,3186,3236,3558,3598,3831,
+    //         4177,4222,4790,4800,4977,5387,5390,6129,6287,6750,6822,7106,
+    //         7310,7421,7851,8286,8292,8987,8997,9168,9171,9757,9772,9775,
+    //         9876,9980,10155,12263,16358,18100,7117,16548,16846,18608,
+    //         22235
+    //     ];
 
-        if ( cdnBox_2.includes( Number(p.d) ) ) {
+    //     if ( cdnBox_1.includes( Number(p.d) ) ) {
+    //         p[0] = p.tmp.w.splice(0,1).join( " " );
+    //         p.a = p.tmp.w.splice(0,1).join( " " );
+    //         p[9] = p.tmp.w.join( " " );
+    //     };
 
-            // .. check structure
-            for ( let i=0; i<p.tmp.w.length; i++ ) {
-                if ( i%2 ) {
-                    if ( !p.tmp.w[i].includes( "hadith" ) )
-                        console.log(p.d);
-                }
-                else {
-                    if ( p.tmp.w[i].includes( "hadith" ) )
-                        console.log(p.d);
-                }
-            }
+    //     let cdnBox_2 = [
+    //         91,108,214,325,343,730,845,957,1096,1106,1111,1125,1137,1194,
+    //         1264,1301,1355,1359,1363,1425,1435,1444,1563,1669,1686,1692,
+    //         1707,1777,1784,1876,1877,1987,2098,2177,2188,2328,2348,2360,
+    //         2374,2405,2467,2514,2516,2529,2559,2560,2568,2653,2671,2911,
+    //         2978,3003,3074,3093,3098,3122,3125,3132,3148,3222,3252,3400,
+    //         3428,3449,3455,3509,3536,3559,3579,3627,3669,3704,3951,4202,
+    //         4360,4746,5016,5130,5337,5576,5870,6098,6200,6260,6569,7082,
+    //         7084,7201,7264,7372,7521,7554,7560,7690,7918,8116,8174,8308,
+    //         8432,8497,8524,8669,8744,8815,8842,8888,10027,10073,10121,
+    //         11152,11160,11519,11674,11741,11804,11876,11914,11930,12182,
+    //         12193,12232,12293,12295,12313,12738,12751,12752,12769,12778,
+    //         12830,12846,12911,12987,13015,13069,13138,13159,13208,13238,
+    //         13217,13146,13125,13115,13376,13388,13401,13422,13463,13481,
+    //         13512,13527,13528,13541,13544,13558,13567,13578,13588,13592,
+    //         13623,13627,13629,13633,13639,14328,14339,14437,14516,14529,
+    //         14563,14662,14695,14703,14724,14908,15119,15198,15342,15974,
+    //         16350,16384,16416,16621,16658,16687,16856,17362,17908,17919,
+    //         17924,17933,17955,18039,18080,18083,18092,18095,18127,18128,
+    //         18143,18183,18203,18207,18217,19134,19306,19553,19593,19627,
+    //         19634,19651,19652,19695,19733,19947,19986,20052,20199,20213,
+    //         20260,20266,20303,20383,20385,20491,20515,20518,20776,20796,
+    //         20815,21100,21482,21601,22005,22233,22439,22607,22728,
+    //         13283,7093,7095,7442,7859,12649,12931,12954,12963,12965,
+    //         13079,13224,13283,13342,13351,13366,13488,13668,13671,13674,
+    //         13685,13686,13689,13699,13707,13731,13753,13761,13762,13833,
+    //         13837,13895,13945,13947,13954,14043,14202,14244,14252,14263,
+    //         14269,14277,14281,14285,14303,15587,19355,19940,327,
+    //     ];
 
-            let tmpCnt: TS.db_item[] = [];
-            let tmpBox: string[] = [];
-            let tmpItem: TS.db_item = {} as any;
-            let cdx = 1;
-            while ( p.tmp.w.length ) {
-                tmpBox = [];
-                tmpBox = p.tmp.w.splice(0,2);
-                if ( tmpBox.length === 1 ) {
-                    tmpCnt[ tmpCnt.length -1 ][9] = tmpBox[0]; 
-                }
-                else {
-                    tmpItem = JSON.parse( JSON.stringify(p) );
-                    tmpItem.d += " - " + cdx;
-                    tmpItem[0] = tmpBox[0];
-                    tmpItem.a = tmpBox[1];
-                    tmpCnt.push( tmpItem );
-                }
-                cdx++;
-            }
+    //     if ( cdnBox_2.includes( Number(p.d) ) ) {
 
-            container[ p.d ] = tmpCnt;
+    //         // .. check structure
+    //         for ( let i=0; i<p.tmp.w.length; i++ ) {
+    //             if ( i%2 ) {
+    //                 if ( !p.tmp.w[i].includes( "hadith" ) )
+    //                     console.log(p.d);
+    //             }
+    //             else {
+    //                 if ( p.tmp.w[i].includes( "hadith" ) )
+    //                     console.log(p.d);
+    //             }
+    //         }
 
-        };
+    //         let tmpCnt: TS.db_item[] = [];
+    //         let tmpBox: string[] = [];
+    //         let tmpItem: TS.db_item = {} as any;
+    //         let cdx = 1;
+    //         while ( p.tmp.w.length ) {
+    //             tmpBox = [];
+    //             tmpBox = p.tmp.w.splice(0,2);
+    //             if ( tmpBox.length === 1 ) {
+    //                 tmpCnt[ tmpCnt.length -1 ][9] = tmpBox[0]; 
+    //             }
+    //             else {
+    //                 tmpItem = JSON.parse( JSON.stringify(p) );
+    //                 tmpItem.d += " - " + cdx;
+    //                 tmpItem[0] = tmpBox[0];
+    //                 tmpItem.a = tmpBox[1];
+    //                 tmpCnt.push( tmpItem );
+    //             }
+    //             cdx++;
+    //         }
+
+    //         container[ p.d ] = tmpCnt;
+
+    //     };
 
     }
 
-    // .. replace container with each cell of cdnBox_2
-    for( let p of Object.keys( container ) ) {
-        db.splice( db.findIndex( x => x.d === p ), 1, ...container[p] )
-    }
+    // // .. replace container with each cell of cdnBox_2
+    // for( let p of Object.keys( container ) ) {
+    //     db.splice( db.findIndex( x => x.d === p ), 1, ...container[p] )
+    // }
 
-    return db.filter( x => x.a );
+    let cdnBox = [
+        [1148,2],[1622,2],[3154,3],[3837,3],[4294,3],[4799,2],[5046,2],
+        [5924,3],[7125,3],[7820,2],[7878,2],[8713,2],[11143,2],[11611,2],
+        [13245,2],[13429,2],[13658,2],[13933,3],[14038,2],[15459,2],
+        [15944,2],[16209,3],[19159,2],[19799,2],[20676,2],[20693,2],
+        [21304,2],[21337,2],[21388,2],[21799,3]
+    ]
+
+    return db.filter( x => typeof x.a === "undefined" );
+
+}
+
+// .. ====================================================================
+
+function tmpProvider_1( tmp_db: TS.db ) {
+
+    let tmp = "";
+    for ( let p of tmp_db ) {
+
+        tmp += "<p>";
+
+        tmp += p.d + "\n";
+        tmp += "<span class='prefix'>" + p[0] + "</span>";
+        if ( p.a ) tmp += "<span class='main'>" + p.a + "</span>";
+        if ( p[9] ) tmp += "<span class='suffix'>" + p[9] + "</span>";
+
+        tmp += "</p>";
+
+    };
+
+    return tmp;
+
+}
+
+// .. ====================================================================
+
+function tmpProvider_2( tmp_db: TS.db ) {
+
+    let tmp = "";
+    for ( let p of tmp_db ) {
+
+        tmp += "<p>";
+
+        tmp += p.d + "\n";
+
+        for ( let q of p.tmp.w ) tmp += "<span>" + q + "</span>";
+
+        tmp += "</p>";
+
+    };
+
+    return tmp;
 
 }
 
